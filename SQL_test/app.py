ca5efed90eb9d0608ae071ff09a89b54c2d48635
AnnaRavkina <98691875+AnnaRavkina@users.noreply.github.com>
2022-05-08 12:42:46 +0300

    Delete SQL_test directory

--- SQL_test/app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, render_template
-import database
-
-app = Flask(__name__)
-
-@app.route("/")
-def hello_world():
-    suppliers = database.get_all_suppliers()
-    return render_template('index.html', suppliers=suppliers)
