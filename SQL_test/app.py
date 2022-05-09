0255554d45805e318b8dfedf9ee0dfbcb2a0ac05
AnnaRavkina <98691875+AnnaRavkina@users.noreply.github.com>
2022-05-09 17:15:28 +0300

    Add files via upload

--- SQL_test/app.py
+++ SQL_test/app.py
@@ -4,6 +4,12 @@ import database
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
+def suppliers():
     suppliers = database.get_all_suppliers()
     return render_template('index.html', suppliers=suppliers)
+
+@app.route("/suppliers/<int:supplier_id>")
+def products(supplier_id):
+    products = database.get_supplier_products(supplier_id)
+    suppliers = database.get_company(supplier_id)
+    return render_template('products.html', products=products, suppliers=suppliers)
--- SQL_test/database.py
+++ SQL_test/database.py
@@ -1,9 +1,9 @@
 import sqlite3
 
-def query(query_text):
+def query(query_text, *param):
     conn = sqlite3.connect('Northwind_small.sqlite')
     cur = conn.cursor()
-    cur.execute(query_text)
+    cur.execute(query_text, param)
 
     column_names= []
     for column in cur.description:
@@ -20,6 +20,13 @@ def query(query_text):
     return dicts
 
 def get_all_suppliers():
-    return query("""SELECT CompanyName, City, Country FROM Supplier ORDER BY CompanyName""")
+    return query("""SELECT * FROM Supplier ORDER BY CompanyName""")
 
 
+def get_supplier_products(supplier_id):
+    return query("""SELECT * FROM Product
+    WHERE SupplierId = ?""", supplier_id)
+
+def get_company(supplier_id):
+    return query("""SELECT CompanyName FROM Supplier
+    WHERE Id = ?""", supplier_id)
